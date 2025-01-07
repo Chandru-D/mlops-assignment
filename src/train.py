71c557d9cb535562be78518f1df40155c007f1ab
Chandru-D <chanchinna0246@gmail.com>
2025-01-07 20:58:17 +0530

    Linting errors cleared

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
+# Save Model
+import joblib
 
 # Sample Training Data
 X = np.array([[1], [2], [3], [4]])
@@ -9,6 +11,4 @@ y = np.array([2, 4, 6, 8])
 model = LinearRegression()
 model.fit(X, y)
 
-# Save Model
-import joblib
 joblib.dump(model, 'model.pkl')
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,3 +3,4 @@ import joblib
 def test_model():
     model = joblib.load('model.pkl')
     assert model.predict([[5]])[0] == 10
+    
