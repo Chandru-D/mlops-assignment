a50ca45e1b8c9252d61ab7208d381d51db3d8234
Chandru-D <chanchinna0246@gmail.com>
2025-01-07 21:15:08 +0530

    Linting errors cleared

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
-from sklearn.linear_model import LinearRegression
-import numpy as np
-# Save Model
 import joblib
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import os
 
 # Sample Training Data
 X = np.array([[1], [2], [3], [4]])
@@ -11,4 +11,26 @@ y = np.array([2, 4, 6, 8])
 model = LinearRegression()
 model.fit(X, y)
 
-joblib.dump(model, 'model.pkl')
+# Get the absolute path to the project root directory
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Save model to the project root
+model_path = os.path.join(project_root, 'model.pkl')
+joblib.dump(model, model_path)
+
+print(f"Model saved to {model_path}")
+
+# from sklearn.linear_model import LinearRegression
+# import numpy as np
+# # Save Model
+# import joblib
+
+# # Sample Training Data
+# X = np.array([[1], [2], [3], [4]])
+# y = np.array([2, 4, 6, 8])
+
+# # Train Model
+# model = LinearRegression()
+# model.fit(X, y)
+
+# joblib.dump(model, 'model.pkl')
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,5 +2,5 @@ import joblib
 
 
 def test_model():
-    model = joblib.load('../src/model.pkl')
+    model = joblib.load('model.pkl')
     assert model.predict([[5]])[0] == 10
