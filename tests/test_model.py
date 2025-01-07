19f4b9f3d52a4aafc60111e799f91568b59f3b85
Chandru-D <chanchinna0246@gmail.com>
2025-01-07 21:38:06 +0530

    Lint error in test model

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,12 +5,10 @@ import os
 def test_model():
     # Define the path to model.pkl relative to the tests folder
     model_path = os.path.join(os.path.dirname(__file__), '../model.pkl')
-    
     # Ensure the file exists before loading
     assert os.path.exists(model_path), f"{model_path} does not exist!"
-    
     # Load the model
     model = joblib.load(model_path)
-    
     # Perform the test
     assert model.predict([[5]])[0] == 10
+    
