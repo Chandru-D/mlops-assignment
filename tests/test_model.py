f54b3457c83dc635e98c6565624765c2d80a773d
Deborah Selvi D <deboselvi@gmail.com>
2025-01-27 20:57:43 +0530

    test model change

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def test_model():
     # Define the path to model.pkl relative to the tests folder
-    model_path = os.path.join(os.path.dirname(__file__), '../model/model.pkl')
+    model_path = os.path.join(os.path.dirname(__file__), '../model.pkl')
     # Ensure the file exists before loading
     # print(os.path.exists(model_path))
     assert os.path.exists(model_path), f"{model_path} does not exist!"
